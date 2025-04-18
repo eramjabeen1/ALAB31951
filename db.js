6a444f69e3722595fbaa2b24e7df07f74b0759b1
Eram Jabeen <contacteramjabeen@gmail.com>
2025-04-18 14:07:16 -0400

    Show refactor: native MongoDB driver vs Mongoose

--- db.js
+++ db.js
@@ -1,3 +1,23 @@
+// This file shows both the original MongoDB native connection and the refactored Mongoose version.
+
+// BEFORE: Using MongoDB native driver (for comparison only)
+// const { MongoClient } = require("mongodb");
+// const uri = process.env.MONGO_URI;
+// const client = new MongoClient(uri);
+
+// async function connectDB() {
+//   try {
+//     await client.connect();
+//     console.log("MongoDB connected (native driver)");
+//   } catch (err) {
+//     console.error("MongoDB connection error:", err);
+//     process.exit(1);
+//   }
+// }
+
+// module.exports = connectDB;
+
+// AFTER: Using Mongoose (refactored version)
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -12,4 +32,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
