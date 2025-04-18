d464af97b55e86e36e831a9fa0ecd4c038326b30
Eram Jabeen <contacteramjabeen@gmail.com>
2025-04-18 13:34:35 -0400

    Refactored index.js to include grades routes

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const connectDB = require("./db"); // <--- bringing in the db.js function
 require("dotenv").config();
 
 const app = express();
+const gradesRoutes = require("./routes/grades");
 const PORT = 3000;
 
 app.use(express.json());
 
 connectDB(); // <--- this line must be present to run the connection
+app.use("/grades", gradesRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Grades Mongoose API running!");
